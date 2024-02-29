642af77dd3990392033e9a500d484c72c0cb1c7a
Rasmus Koudal Ferst <rasm427u@zbc.dk>
2024-02-29 12:57:02 +0100

    Helped Jakob

---
+++ HelpJakobSRP/HelpJakobSRP/Program.cs
@@ -0,0 +1,64 @@
+﻿public class Message
+{
+    string to, from, body, subject, cc;
+
+    public Message(string to, string from, string body, string subject, string cc)
+    {
+        this.to = to;
+        this.from = from;
+        this.body = body;
+        this.subject = subject;
+        this.cc = cc;
+    }
+
+    public string To { get => to; set => to = value; }
+    public string From { get => from; set => from = value; }
+    public string Body { get => body; set => body = value; }
+    public string Subject { get => subject; set => subject = value; }
+    public string Cc { get => cc; set => cc = value; }
+}
+
+public interface IMessageHandler
+{
+    void SendMessage(Message m);
+    void SendMessageToAll(string[] to, Message m);
+}
+
+public class SMTPMessageHandler : IMessageHandler
+{
+    public void SendMessage(Message m)
+    {
+
+    }
+    public void SendMessageToAll(string[] to, Message m)
+    {
+
+    }
+}
+
+public class VMessageHandler : IMessageHandler
+{
+    public void SendMessage(Message m)
+    {
+
+    }
+    public void SendMessageToAll(string[] to, Message m)
+    {
+
+    }
+}
+
+public interface IConverter
+{
+    string Convert(string message);
+}
+
+public class HTMLConverter : IConverter
+{
+    public string Convert(string message)
+    {
+        return $"<html><body>{message}<html><body>";
+    }
+}
+
+
